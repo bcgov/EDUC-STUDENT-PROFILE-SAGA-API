590ecd6e1126428defeb982a480b04e0df392fd3
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-06-30 08:45:12 -0700

    adding JSON ignore..

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/struct/base/Event.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/struct/base/Event.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.api.student.profile.saga.struct.base;
 
 import ca.bc.gov.educ.api.student.profile.saga.constants.EventOutcome;
 import ca.bc.gov.educ.api.student.profile.saga.constants.EventType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Event {
   private EventType eventType;
   private EventOutcome eventOutcome;
