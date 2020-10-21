f2a313b6c04acc72a062e68aa1885a32e1bc8957
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-10-21 11:22:17 -0700

    Fix for returning two way twin records.

--- api/src/test/java/ca/bc/gov/educ/api/student/profile/saga/controller/StudentProfileSagaControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/student/profile/saga/controller/StudentProfileSagaControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StudentProfileSagaControllerTest {
   public static final String PAYLOAD_STR = "  \"studentProfileRequestID\": \"ac335214-7252-1946-8172-589e58000004\",\n" +
       "  \"createUser\": \"om\",\n" +
