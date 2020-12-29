7cb021732e5b5d95aa4bc131c196133ccbddb0a8
mishraomp <omprakashmishra3978@gmail.com>
2020-12-28 17:06:53 -0800

    code changes to make sure all injections are proxied and working as per spring guidelines and not using functional model which does not inject objects properly to work with AOP.

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/config/StudentProfileSagaMVCConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/config/StudentProfileSagaMVCConfig.java
@@ -3,11 +3,15 @@ package ca.bc.gov.educ.api.student.profile.saga.config;
 import lombok.AccessLevel;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
@@ -25,5 +29,16 @@ public class StudentProfileSagaMVCConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(studentProfileSagaInterceptor).addPathPatterns("/**");
+  }
+
+  @Bean
+  public Executor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5);
+    executor.setMaxPoolSize(10);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("async-task-");
+    executor.initialize();
+    return executor;
   }
 }
