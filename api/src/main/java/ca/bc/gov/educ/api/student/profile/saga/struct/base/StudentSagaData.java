884149a9cda8e2715c4e93bcb0375cfc9e3aa2c9
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-01-06 10:29:06 -0800

    code changes for PEN-883

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/struct/base/StudentSagaData.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/struct/base/StudentSagaData.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.educ.api.student.profile.saga.struct.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StudentSagaData {
   private String studentID;
   private String pen;
