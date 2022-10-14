1e5cbc2b30a8b42dd10f81201d61cd4da36d4a98
SoLetsDev <derek.so@gov.bc.ca>
2022-10-14 14:39:59 -0700

    HD-14588 fix to add index and change saga event deletion logic

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/repository/SagaEventRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/repository/SagaEventRepository.java
@@ -2,6 +2,10 @@ package ca.bc.gov.educ.api.student.profile.saga.repository;
 
 import ca.bc.gov.educ.api.student.profile.saga.model.v1.Saga;
 import ca.bc.gov.educ.api.student.profile.saga.model.v1.SagaEvent;
+import java.time.LocalDateTime;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +20,9 @@ public interface SagaEventRepository extends CrudRepository<SagaEvent, UUID> {
   Optional<SagaEvent> findBySagaAndSagaEventOutcomeAndSagaEventStateAndSagaStepNumber(Saga saga, String eventOutcome, String eventState, int stepNumber);
 
   Optional<SagaEvent> findBySagaAndSagaEventOutcomeAndSagaEventState(Saga saga, String eventOutcome, String eventState);
+
+  @Transactional
+  @Modifying
+  @Query(value = "delete from STUDENT_PROFILE_SAGA_EVENT_STATES e where exists(select 1 from STUDENT_PROFILE_SAGA s where s.SAGA_ID = e.SAGA_ID and s.CREATE_DATE <= :createDate)", nativeQuery = true)
+  void deleteBySagaCreateDateBefore(LocalDateTime createDate);
 }
--- api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/repository/SagaRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/saga/repository/SagaRepository.java
@@ -4,6 +4,9 @@ import ca.bc.gov.educ.api.student.profile.saga.model.v1.Saga;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,5 +23,8 @@ public interface SagaRepository extends JpaRepository<Saga, UUID>, JpaSpecificat
 
   Optional<Saga> findByPenRequestIdAndStatusInAndSagaName(UUID penRequestId, List<String> statuses, String sagaName);
 
-  List<Saga> findAllByCreateDateBefore(LocalDateTime createDate);
+  @Transactional
+  @Modifying
+  @Query("delete from Saga where createDate <= :createDate")
+  void deleteByCreateDateBefore(LocalDateTime createDate);
 }
